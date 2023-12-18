988cea13ae15c15b4f48ce3d1d862de7e2d56c0f
user <yasenovich.mikhail@mail.ru>
2023-12-18 16:38:35 +0300

    added skip and unavailable plots

--- Downloads video/downloads_video_by_day.py
+++ Downloads video/downloads_video_by_day.py
@@ -6,13 +6,39 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 
-select_video_download = """select date_trunc('day', download_date), count(*)
-from tm_video_downloads
-where date_trunc('day', download_date) between '2023-03-31 23:59:59' and '2023-04-30 23:59:59'
-group by date_trunc('day', download_date)
-order by date_trunc('day', download_date) desc"""
+select_true_order_actions = """select date_trunc('day', server_date), count(*)
+from tm_order_actions
+where server_date between '2023-11-30 23:59:59' AND '2023-12-31 23:59:59'
+and is_success = 'true'
+and metrics_amount_before != '-1'
+group by date_trunc('day', server_date)
+order by date_trunc('day', server_date) desc"""
 
-video_april = pd.read_sql(select_video_download, conn)
+select_false_order_actions = """select date_trunc('day', server_date), count(*)
+from tm_order_actions
+where server_date between '2023-11-30 23:59:59' AND '2023-12-31 23:59:59'
+and is_success = 'false'
+group by date_trunc('day', server_date)
+order by date_trunc('day', server_date) desc"""
+
+select_unavailable_order_actions = """select date_trunc('day', server_date), count(*)
+from tm_order_actions
+where server_date between '2023-11-30 23:59:59' AND '2023-12-31 23:59:59'
+and metrics_amount_before = '-1'
+group by date_trunc('day', server_date)
+order by date_trunc('day', server_date) desc;"""
+
+select_skip_order_actions = """select date_trunc('day', server_date), count(*)
+from tm_order_actions
+where server_date between '2023-11-30 23:59:59' AND '2023-12-31 23:59:59'
+and metrics_amount_before = '-2'
+group by date_trunc('day', server_date)
+order by date_trunc('day', server_date) desc;"""
+
+true_order_actions_december = pd.read_sql(select_true_order_actions, conn)
+false_order_actions_december = pd.read_sql(select_false_order_actions, conn)
+unavailable_order_actions_december = pd.read_sql(select_unavailable_order_actions, conn)
+skip_order_actions_december = pd.read_sql(select_skip_order_actions, conn)
 timeFmt = mdates.DateFormatter('%d')
 
 # months = mdates.MonthLocator()
@@ -22,12 +48,15 @@ fig, ax = plt.subplots(figsize=(12, 8), layout='constrained')
 ax.xaxis.set_major_formatter(timeFmt)
 ax.xaxis.set_major_locator(ticker.AutoLocator())
 ax.yaxis.set_major_locator(ticker.AutoLocator())
-plt.xlabel("April", fontsize=14, fontweight="bold")
+plt.xlabel("December", fontsize=14, fontweight="bold")
 plt.ylabel("Amount", fontsize=14, fontweight="bold")
-plt.title("Video Downloads by day", fontsize=14, fontweight="bold")
-plt.locator_params (axis='x', nbins= 30 )
-plt.locator_params (axis='y', nbins= 20 )
+plt.title("Order actions", fontsize=14, fontweight="bold")
+plt.locator_params (axis='x', nbins= len(true_order_actions_december['date_trunc']))
+plt.locator_params (axis='y', nbins= 20)
 plt.grid(True)
-plt.plot(video_april['date_trunc'], video_april['count'], '*-.g', alpha=0.7, lw=1, mec='r', mew=3, ms=6)
-d = video_april.describe()
+plt.plot(true_order_actions_december['date_trunc'], true_order_actions_december['count'], '-.g', alpha=0.7, lw=1, mec='r', mew=3, ms=6, label='true')
+plt.plot(false_order_actions_december['date_trunc'], false_order_actions_december['count'], '-.y', alpha=0.7, lw=1, mec='r', mew=3, ms=6, label='false')
+plt.plot(unavailable_order_actions_december['date_trunc'], unavailable_order_actions_december['count'], '-.b', alpha=0.7, lw=1, mec='r', mew=3, ms=6, label='unavailable')
+plt.plot(skip_order_actions_december['date_trunc'], skip_order_actions_december['count'], '-.r', alpha=0.7, lw=1, mec='r', mew=3, ms=6, label='skip')
+plt.legend(loc='best')
 plt.show()
