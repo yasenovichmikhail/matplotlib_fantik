7fef955e4560b4ec4ce048204a054dff554706f5
user <yasenovich.mikhail@mail.ru>
2024-05-13 16:43:21 +0300

    modified app_promotion chart

--- Users/app_promotion.py
+++ Users/app_promotion.py
@@ -2,12 +2,13 @@ from config import *
 
 
 def apps_promotion(data_frame):
+    text_kwargs = dict(ha='center', va='center', fontsize=12, fontweight='bold')
     fig, ax = plt.subplots(figsize=(14, 10), layout='constrained')
     splot = sns.barplot(data_frame, x='app_name', y='amount', color='c', width=0.5)
     plt.bar_label(splot.containers[0], size=12, fontweight="bold")
-    plt.ylabel("Installs", fontsize=14, fontweight="bold")
-    plt.xlabel("App name", fontsize=14, fontweight="bold")
-    plt.title("Apps promotion", fontsize=14, fontweight="bold")
+    plt.ylabel("Log In", **text_kwargs)
+    plt.xlabel("App name", **text_kwargs)
+    plt.title("Apps promotion", **text_kwargs)
     plt.grid(True)
     plt.show()
 
