4328f8ab8a1adbe80cd73b69edf496896fbf6b42
user <yasenovich.mikhail@mail.ru>
2024-09-10 15:35:14 +0300

    added choose amount of countries parameter

--- Users/all_users_by_country.py
+++ Users/all_users_by_country.py
@@ -1,21 +1,31 @@
 from config import *
 
 
-def users_by_country_month(users_top_country_month):
+def users_by_country_month(number_of_countries):
+
+    select_users_by_country = f"""select country_iso, count(*) cnt
+        from tm_users
+        where actual_date between '{DATE1}' and '{DATE2}'
+        group by country_iso
+        order by cnt desc
+        limit {number_of_countries}"""
+
+    all_users_by_country = pd.read_sql(select_users_by_country, CONN)
+
     fig = plt.figure(figsize=(14, 10))
-    splot = sns.barplot(users_top_country_month, x='country_iso', y='cnt', color='c', width=0.8)
-    plt.bar_label(splot.containers[0], size=14)
+    splot = sns.barplot(all_users_by_country, x='country_iso', y='cnt', color='c', width=0.8)
+    plt.bar_label(splot.containers[0], size=number_of_countries - 1)
     text_kwargs = dict(ha='center', va='center', fontsize=12, fontweight='bold')
 
     top_countries_list = []
-    for i in users_top_country_month['country_iso']:
+    for i in all_users_by_country['country_iso']:
         for key, value in COUNTRIES.items():
             if i == key:
                 top_countries_list.append(value)
 
     plt.xlabel("Country", **text_kwargs)
     plt.ylabel("Number of users", **text_kwargs)
-    plt.title("Top 15 countries", **text_kwargs)
+    plt.title(f"Top {number_of_countries} countries", **text_kwargs)
     plt.grid(True)
     plt.legend(
         loc='upper right', labels=top_countries_list, prop={'weight': 'bold'}
@@ -24,5 +34,4 @@ def users_by_country_month(users_top_country_month):
 
 
 if __name__ == '__main__':
-    all_users_by_country = pd.read_sql(SELECT_USERS_BY_COUNTRY, CONN)
-    users_by_country_month(all_users_by_country)
+    users_by_country_month(10)
